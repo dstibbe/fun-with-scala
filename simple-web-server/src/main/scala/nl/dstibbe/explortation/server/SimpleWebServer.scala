15ee708899a6f197e45168eaea81c7a876d10eab
David Stibbe <dstibbe@gmail.com>
2016-01-26 20:56:27 +0100

    Initial version based on examples

---
+++ simple-web-server/src/main/scala/nl/dstibbe/explortation/server/SimpleWebServer.scala
@@ -0,0 +1,44 @@
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl._
+import akka.http.scaladsl.Http
+import akka.actor.ActorSystem
+import scala.concurrent.Future
+
+
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model._
+
+
+
+object AkkaHttpMicroservice extends App {
+
+	implicit val system = ActorSystem()
+	implicit val materializer = ActorMaterializer()
+	 
+	val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
+	  Http().bind(interface = "localhost", port = 9000)
+	
+	val requestHandler: HttpRequest => HttpResponse = {
+	  case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+		HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
+		  "<html><body>Hello world!</body></html>"))
+	 
+	  case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
+		HttpResponse(entity = "PONG!")
+	 
+	  case HttpRequest(GET, Uri.Path("/crash"), _, _, _) =>
+		sys.error("BOOM!")
+	 
+	  case _: HttpRequest =>
+		HttpResponse(404, entity = "Unknown resource!")
+	}
+
+	
+	val bindingFuture: Future[Http.ServerBinding] =
+	  serverSource.to(Sink.foreach { connection => // foreach materializes the source
+		println("Accepted new connection from " + connection.remoteAddress)
+		
+		 connection handleWithSyncHandler requestHandler
+		
+	  }).run()
+}
