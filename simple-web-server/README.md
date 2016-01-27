72d467cb60f33ff744b4428653c5280424302efc
David Stibbe <dstibbe@gmail.com>
2016-01-28 00:58:50 +0100

    Refactored

---
+++ simple-web-server/README.md
@@ -0,0 +1,19 @@
+Simple web server
+-----------------
+
+Starting the server:
+====================
+
+```
+sbt "run [{location}]"
+```
+Location is the root location for the server. Optional, defaults to "."
+
+Urls:
+====================
+/test     - hello world
+/ping     - ping pong
+/crash    - crash
+/         - listing
+
+
--- simple-web-server/src/main/scala/nl/dstibbe/explortation/server/SimpleWebServer.scala
+++ simple-web-server/src/main/scala/nl/dstibbe/explortation/server/SimpleWebServer.scala
@@ -9,28 +9,46 @@ import scala.concurrent.Future
 
 import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model._
-
+import java.io.File
 
 
 object SimpleWebServer extends App {
-
+	println("Args: " +  (args mkString ", "))
+	
 	implicit val system = ActorSystem()
 	implicit val materializer = ActorMaterializer()
 	 
+	val rootLocation = if (args.isEmpty) "." else args(0)
+		 
+	println("Root location: " + rootLocation)
+		 
 	val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
 	  Http().bind(interface = "localhost", port = 9000)
 	
 	val requestHandler: HttpRequest => HttpResponse = {
-	  case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
-		HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
-		  "<html><body>Hello world!</body></html>"))
-	 
-	  case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
-		HttpResponse(entity = "PONG!")
-	 
-	  case HttpRequest(GET, Uri.Path("/crash"), _, _, _) =>
-		sys.error("BOOM!")
-	 
+	  case HttpRequest(GET, uri:Uri, _, _, _) =>  
+		uri match {
+			case Uri.Path("/test") =>
+				HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
+				"<html><body>Hello world!</body></html>"))
+		 
+			case Uri.Path("/ping") =>
+				HttpResponse(entity = "PONG!")
+		 
+			case Uri.Path("/crash") =>
+				sys.error("BOOM!")
+				
+			case Uri.Path("/") => 
+				HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
+				"<html><body>" + (new File(rootLocation).listFiles() mkString "\n<br/>") + "</body></html>"))				
+				
+			case Uri.Path(path:String) =>
+				HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
+				"<html><body>Requested file: " + rootLocation + path + "</body></html>"))
+			case _ =>
+				HttpResponse(404, entity = "Unknown resource!")
+		}
+		
 	  case _: HttpRequest =>
 		HttpResponse(404, entity = "Unknown resource!")
 	}
@@ -43,4 +61,6 @@ object SimpleWebServer extends App {
 		 connection handleWithSyncHandler requestHandler
 		
 	  }).run()
+	  
+	  println("Server started")
 }
